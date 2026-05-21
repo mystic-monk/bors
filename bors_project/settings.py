9c151b43dd4b3035b5091e47bf8992bcb6ab1b7f
Ankush Arya <ankush.arya@nationaltransport.com>
2026-05-21 19:59:20 +0100

    adopted for render

--- bors_project/settings.py
+++ bors_project/settings.py
@@ -1,12 +1,13 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'change-this-in-production-use-env-variable'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'change-this-in-production-use-env-variable')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -50,11 +51,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bors_project.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -70,6 +73,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'returns' / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
